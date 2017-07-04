c058d907f51bd8434450fd9f777d61986c1d7a6c
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2017-07-04 23:39:16 +0700

    first pushing

---
+++ frontend/asset/js/main.js
@@ -0,0 +1,34 @@
+!function(e){
+	var o = new ScrollMagic.Controller,
+		t = ["#slide01","#slide02","#slide03"],
+		r = ["#slide01 header","#slide02 header","#slide03 header"],
+		n = ["#cb01","#cb02","#cb03"];
+
+	if(Modernizr.touch){
+		r.forEach(function(e,t){
+		var r = t+1;
+		new ScrollMagic.Scene({triggerElement:e,offset:-95}).setClassToggle("#slide0"+r,"is-active").addTo(o)}),
+			n.forEach(function(t,r){
+			var n = e(t).attr("id");
+			new ScrollMagic.Scene({triggerElement:t,triggerHook:.75}).setClassToggle("#"+n,"is-active").on("enter",function(o){
+				e("nav").attr("class","is-light")}).addTo(o)}),
+			t.forEach(function(t,r){
+			new ScrollMagic.Scene({triggerElement:t}).on("enter",function(o){
+				e("nav").removeAttr("class")}).addTo(o)}),t.forEach(function(t,r){
+				var n = e(t).find(".bcg");
+				new ScrollMagic.Scene({triggerElement:t,triggerHook:1,duration:"100%"}).setTween(TweenMax.from(n,1,{y:"-40%",autoAlpha:.3,ease:Power0.easeNone})).addTo(o)});
+				var a = new TimelineMax;
+				a.to(e("#intro header, .scroll-hint"),
+				.2,{autoAlpha:0,ease:Power1.easeNone}).to(e("#intro .bcg"),1.4,{y:"20%",ease:Power1.easeOut},"-=0.2").to(e("#intro"),.7,{autoAlpha:.5,ease:Power1.easeNone},"-=1.4"),
+				new ScrollMagic.Scene({
+					triggerElement:"#intro",triggerHook:0,duration:"100%"}).setTween(a).addTo(o),o.scrollTo(function(e){
+					TweenMax.to(window,1,{scrollTo:{y:e},ease:Power1.easeInOut})}),
+
+				e(document).on("click","a[href^='#']",function(t){
+					var r = e(this).attr("href");
+							e(r).length > 0 &&(t.preventDefault(),
+							o.scrollTo(r),
+							window.history&&window.history.pushState&&history.pushState("",document.title,r)
+				)}
+	)}
+}(jQuery);
---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Kinema Visual Supply</title>
+    <meta name="description" content="Kinema Visual Supply Photography And Cinematograpy">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="frontend/asset/img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="frontend/asset/img/fav/favicon.ico" type="image/x-icon">
+    <!-- Google Authorship and Publisher Markup -->
+    <link rel="author" href="">
+    <link rel="publisher" href=""> 
+    <!-- <link rel="stylesheet" href="frontend/asset/css/styles.css"> -->
+    <link rel="stylesheet" href="frontend/asset/css/kinema.css">
+    <link rel="stylesheet" href="frontend/asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend/asset/css/preloader.css">
+</head>
+<body class="kinema-visual-supply">
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
+    <div class="container-fluid">
+        <section class="main-section-homepage">
+            <div class="row">
+                <div class="col-sm-12 col-lg-12">
+                    <!-- <div id="intro" class="header-container fs"> -->
+                        <!-- <div class="bcg"> -->
+                            <!-- <header class="wrapper">  -->
+                                <!-- <h1 class="title"><strong>KINEMA<strong></h1>  -->
+                                <img src="frontend/asset/img/images/kinemavisual.jpg" class="img-responsive slide01">
+                            <!-- </header>  -->
+                        <!-- </div> -->
+                    <!-- </div> -->
+                </div>
+            </div>
+        </section>
+        <!--nav>
+            <ul>
+                <li><a href="#intro"><span class="nav-dot"></span><span class="nav-label">Intro</span></a></li>
+                <li><a href="#slide01"><span class="nav-dot"></span><span class="nav-label">Images</span></a></li>
+                <li><a href="#slide02"><span class="nav-dot"></span><span class="nav-label">Position and Timing</span></a></li>
+                <li><a href="#slide03"><span class="nav-dot"></span><span class="nav-label">Download</span></a></li>
+            </ul>
+        </nav>-->
+        <section class="main-section-aboutme">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="col-sm-12 col-lg-6">
+                        <div class="col-lg-9">
+                            <img src="frontend/asset/img/images/slrcamera.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="col-lg-10 col-sm-12 space-testiaboutme">
+                            <div class="form-group-bottom-25">
+                                <h2 class="text-centerred">My Name is <strong>Ringgo</strong></h2>
+                            </div>
+                            <p class="aboutme">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt   
+                                mollit anim id est laborum. sed ut perspiciatis unde omnis iste
+                                natus erorr sit volupatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+                                veritatis et quasi voluptas sit aspernatur aut odit aut fugit,sed quia cosnequuntur magni
+                                dolores eos qui rationae voluptatem sequi nesciunt.
+                            </p>
+                            <div class="form-group">
+                                <p class="aboutme">
+                                    ipsum quia dolor sit amet, consectetur,adipisci velit, sed quaia non numquam eius modi tempora incidunt ut 
+                                    labore et dolore magnam aliquam quaerat voluptatem.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- <div class="main-container">
+            <article id="cb01" class="content-block">
+                <div class="wrapper">
+                    <header class="slideInUp">
+                        <h1>SIMPLIFY</h1>
+                    </header>
+                    <section>
+                        <p class="slideInUp slideInUp2">Simplify your content to a MINIMUM.</p>
+                    </section>
+                </div> 
+            </article>
+            
+            <article id="slide01" class="slide fs">
+                <div class="bcg"></div>
+                <div class="wrapper">
+                    <header class="slideInUp">
+                        <h1>A picture is worth a thousand words</h1>
+                    </header>
+                    <section>
+                        <p class="slideInUp slideInUp2">Let the images do the talking and use text for short summaries.</p>
+                    </section>
+                </div> 
+            </article>
+
+            <article id="cb02" class="content-block">
+                <div class="wrapper">
+                    <header class="slideInUp">
+                        <h1>FOCUS</h1>
+                    </header>
+                    <section>
+                        <p class="slideInUp slideInUp2">Focus on the most IMPORTANT thing. The one thing!</p>
+                    </section>
+                </div> 
+            </article>
+
+            <article id="slide02" class="slide fs">
+                <div class="bcg"></div>
+                <div class="wrapper">
+                    <header class="slideInUp">
+                        <h1>Position and Timing</h1>
+                    </header>
+                    <section>
+                        <p class="slideInUp slideInUp2">Position elements based on the background image and trigger animation at the right time.</p>
+                        <p class="slideInUp slideInUp3">Use different triggerElements in combination with offsets to be in full control.</p>
+                    </section>
+                </div> 
+            </article>
+
+            <article id="cb03" class="content-block">
+                <div class="wrapper">
+                    <header class="slideInUp">
+                        <h1>ASK</h1>
+                    </header>
+                    <section>
+                        <p class="slideInUp slideInUp2">You don't ask, you don't get. Always end with CALL TO ACTION.</p>
+                    </section>
+                </div> 
+            </article> -->
+
+       <!--<article id="slide03" class="slide fs">
+                <div class="bcg"></div>
+                <div class="wrapper">
+                    <header class="slideInUp">
+                        <a href="https://ihatetomatoes.net/blog/" title="Petr Tichy's Front End Development Blog">
+                        </a>
+                    </header>
+                </div> 
+            </article> -->
+        </div> <!-- #main-container -->
+        <div class="footer-container">
+            <footer class="wrapper">
+            </footer>
+        </div>
+        <script src="frontend/asset/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="frontend/asset/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <script src="frontend/asset/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="frontend/libs/gsap/1.18.2/TweenMax.min.js"></script>
+        <script src="frontend/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+        <script src="frontend/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
+        <script src="frontend/libs/gsap/1.18.2/plugins/ScrollToPlugin.min.js"></script>
+        <script src="frontend/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+        <script src="frontend/libs/Loadgo/loadgo.min.js"></script>
+        <script src="frontend/asset/js/bootstrap.min.js"></script>
+        <script src="frontend/asset/js/main.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='frontend/asset/js/www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-839919-3','auto');ga('send','pageview');
+
+            $(document).ready(function() {
+                setTimeout(function(){
+                    $('body').addClass('loaded');
+                }, 3500);
+            });
+        </script>
+    </div><!-- container fluid -->
+    </body>
+</html>
